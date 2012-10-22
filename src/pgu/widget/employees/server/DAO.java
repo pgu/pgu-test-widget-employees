cd6959c5ea3f78b98ca9746c7776c65ec33963c9
Pascal Guilcher <guilcher.p@sfeir.com>
2012-10-22 10:30:41 +0200

    ..

--- src/pgu/widget/employees/server/DAO.java
+++ src/pgu/widget/employees/server/DAO.java
@@ -17,24 +17,24 @@ public class DAO {
             String picture = null;
 
             if (i == 0) {
-                picture = "http://lh5.ggpht.com/_7ZYqYi4xigk/SaXJbI77n-I/AAAAAAAAEeA/DeV-BvKsa0c/s288/sergey_brin.jpg";
+                picture = "img/it_crowd_1.png";
                 technos.add(Technos.GWT);
                 technos.add(Technos.CLOJURE);
 
             } else if (i == 1) {
-                picture = "http://ideaelevator.co/wp/wp-content/uploads/2011/01/petergriffinbh9.jpg";
+                picture = "img/it_crowd_2.png";
                 technos.add(Technos.JS);
                 technos.add(Technos.JAVA);
                 technos.add(Technos.NOSQL);
 
             } else if (i == 2) {
-                picture = "http://fc08.deviantart.net/fs71/f/2011/059/4/2/homer_simpson_by_mdiaz13-d3alpqr.jpg";
+                picture = "img/homer.png";
                 technos.add(Technos.GWT);
                 technos.add(Technos.JAVA);
                 technos.add(Technos.SQL);
 
             } else if (i == 3) {
-                picture = "http://www.nataliemaclean.com/blog/wp-content/uploads/2011/03/BradPitt1.jpg";
+                picture = "img/peter.png";
                 technos.add(Technos.GWT);
                 technos.add(Technos.CLOJURE);
                 technos.add(Technos.NOSQL);
